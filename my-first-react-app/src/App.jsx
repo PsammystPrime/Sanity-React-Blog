18898557fe06c8e9d6ee4d05017c8e21ea7b8bdd
PsammystPrime <samuelprime384@gmail.com>
2024-03-24 22:42:06 +0300

    Create contact comp

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -140,7 +140,7 @@ export function Authors() {
   )
 }
 
-function App() {
+export function Featured() {
   return(
     <div className="wrapper row3">
   <section className="hoc container clear"> 
@@ -185,11 +185,61 @@ function App() {
   )
 }
 
-function App() {
+export function Contact() {
   return(
-    
-  )}
-/* <div id= "slides" classNameName="bgded overlay">
+  <div className="wrapper row4 bgded overlay">
+    <div id="footer" className="hoc clear"/> 
+      <div className="one_third first">
+        <h6 className="heading">Quis vestibulum ligula</h6>
+        <ul className="nospace linklist contact">
+          <li id={32}><i className="fa fa-map-marker"></i>
+            <address>
+            Street Name &amp; Number, Town, Postcode/Zip
+            </address>
+          </li>
+          <li id={33}><i className="fa fa-phone"></i> +00 (123) 456 7890</li>
+          <li id={34}><i className="fa fa-fax"></i> +00 (123) 456 7890</li>
+          <li id={35}><i className="fa fa-envelope-o"></i> info@domain.com</li>
+        </ul>
+      </div>
+      <div className="one_third">
+        <h6 className="heading">Pharetra eget nulla</h6>
+        <p className="nospace btmspace-30">Feugiat tellus orci eget maximus est posuere quis nunc ornare erat lorem.</p>
+        <form method="post" action="#">
+          <fieldset>
+            <legend>Newsletter:</legend>
+            <input className="btmspace-15" type="text" value="" placeholder="Name" />
+            <input className="btmspace-15" type="text" value="" placeholder="Email" />
+            
+            <button type="submit" value="submit">Submit</button>
+          </fieldset>
+        </form>
+      </div>
+      <div className="one_third">
+        <h6 className="heading">Viverra justo nec maximus</h6>
+        <ul className="nospace linklist">
+          <li id={42}>
+            <article>
+              <h2 className="nospace font-x1"><a href="#">Dictum vivamus condimentum</a></h2>
+              <time className="font-xs block btmspace-10" /*datetime="2045-04-06"*/ >Friday, 6<sup>th</sup> April 2045</time>
+              <p className="nospace">Velit consequat sit amet praesent consectetur lacinia nulla vitae [&hellip;]</p>
+            </article>
+          </li>
+          <li id={43}>
+            <article>
+              <h2 className="nospace font-x1"><a href="#">Massa arcu quis imperdiet</a></h2>
+              <time className="font-xs block btmspace-10" /*datetime=2045-04-05"*/ >Thursday, 5<sup>th</sup> April 2045</time>
+              <p className="nospace">Bibendum ipsum mattis a mauris ut nibh non sem iaculis finibus nullam [&hellip;]</p>
+            </article>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+  }
+
+
+ {/* <div id= "slides" classNameName="bgded overlay">
   <div  classNameName="hoc clear"> 
 
     <div classNameName="flexslider basicslider">
@@ -221,7 +271,7 @@ function App() {
       </ul>
     </div>
   </div>
-</div> */
+</div> */}
   
 
 
