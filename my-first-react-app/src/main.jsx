fa603a32c3cac31adfba6af904c0fbfd2caa7e01
PsammystPrime <samuelprime384@gmail.com>
2024-03-25 16:05:05 +0300

    Add router to pages

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import App from './App'
 import './styles/layout.css'
+import Featured from './components/Featured.jsx'
+import All from './components/All.jsx'
+
+const router = createBrowserRouter([
+{
+  path: '/',
+  element: <App/>
+},
+{
+  path: 'featured',
+  element: <Featured/>
+},
+{
+  path: 'all',
+  element: <All/>
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
