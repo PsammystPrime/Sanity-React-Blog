aba8940ee95fc8fca46d07c6caab4c9be2c382e9
PsammystPrime <samuelprime384@gmail.com>
2024-04-03 16:05:39 +0300

    Add singlepost page to react router

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import App from './App'
-import './styles/layout.css'
-import Featured from './components/Featured.jsx'
-import All from './components/All.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./styles/layout.css";
+import Featured from "./components/Featured.jsx";
+import All from "./components/All.jsx";
+import { SinglePost } from "./components/article.jsx";
 
 const router = createBrowserRouter([
-{
-  path: '/',
-  element: <App/>
-},
-{
-  path: 'featured',
-  element: <Featured/>
-},
-{
-  path: 'all',
-  element: <All/>
-}
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "featured",
+    element: <Featured />,
+  },
+  {
+    path: "all",
+    element: <All />,
+  },
+  {
+    path: "/post/:slug",
+    element: <SinglePost />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
