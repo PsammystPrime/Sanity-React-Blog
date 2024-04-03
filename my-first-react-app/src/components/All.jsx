d70114794bfd2273e513f6c0b3f3e3241ec9cebf
PsammystPrime <samuelprime384@gmail.com>
2024-04-03 14:42:48 +0300

    Fix blogs display error
    Changed the children param from [index] to [0]

--- my-first-react-app/src/components/All.jsx
+++ my-first-react-app/src/components/All.jsx
@@ -1,13 +1,15 @@
-import { Nav1, Nav2, Contact, Footer, Return } from "../App"
-import { Link } from "react-router-dom"
-import { useState, useEffect } from "react"
-import sanityClient from '../client'
+import { Nav1, Nav2, Contact, Footer, Return } from "../App";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import sanityClient from "../client";
 
-export function Post(){
-    const[postData, setPost] = useState(null)
+export function Post() {
+  const [postData, setPost] = useState(null);
 
-    useEffect(()=>{
-        sanityClient.fetch( `*[_type == 'post' ]{
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == 'post' ]{
             title,
             slug,
             mainImage{
@@ -17,53 +19,55 @@ export function Post(){
                 },
                 alt
             },
-            body,author
-        }`)
-        .then((data)=>setPost(data, console.log(data)))
-        .catch(console.error)
-    }, [])
+            body,
+            author
+        }`
+      )
+      .then((data) => setPost(data, console.log(data)))
+      .catch(console.error);
+  }, []);
 
-
-return(
+  return (
     <main>
-        <section>
-            <h1>Welcome to my blogs</h1>
-            <h2>Have a great read</h2>
-            <div>
-                {postData && postData.map((post, index)=>(
-                <article key={index}>
-                    <Link to={'/post/' + post.slug.current} key={post.slug.current}>
-                    <span key ={index}>
-                        <img 
-                         src={post.mainImage.asset.url}
-                         alt={post.mainImage.alt} 
-                        />
-                        <span>
-                            <h3>{post.title}</h3>
-                            <span>{post.body[index].children[index].text}</span>
-                        </span>
-                        
+      <section className="container">
+        <h1>Welcome to my blogs</h1>
+        <h2>Have a great read</h2>
+        <div className="posts">
+          {postData &&
+            postData.map((post, index) => (
+              <article className="article" key={index}>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span key={index}>
+                    <img
+                      className="pic"
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                    />
+                    <span>
+                      <h3 className="title">{post.title}</h3>
+                      <span className="info">
+                        {post.body[0].children[0].text}
+                      </span>
                     </span>
-                    </Link>
-                </article>
-                ))}
-            </div>
-        
-        </section>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
     </main>
-)
+  );
 }
 
-export default function All(){
-    return (
-        <main>
-            <Nav1></Nav1>
-            <Nav2></Nav2>
-            <Post/>
-            <Contact></Contact>
-            <Footer></Footer>
-            <Return></Return>
-
-        </main>
-        )
+export default function All() {
+  return (
+    <main>
+      <Nav1></Nav1>
+      <Nav2></Nav2>
+      <Post />
+      <Contact></Contact>
+      <Footer></Footer>
+      <Return></Return>
+    </main>
+  );
 }
