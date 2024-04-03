05ca5bd1c72f65f21be25e8e89b455e1003dc7af
PsammystPrime <samuelprime384@gmail.com>
2024-04-03 17:37:44 +0300

    Fetch individual article data using grok and slug

---
+++ my-first-react-app/src/components/article.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client";
+
+export function SinglePosts() {
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == '${slug}']{
+    title,
+    'body': body[0].children[0].text,
+    slug,
+    mainImage{
+        asset->{
+            _id,
+            url
+        }
+    },
+    'author': author->name
+}`
+      )
+      .then((data) => setSinglePost(data[0], console.log(data)))
+      .catch(console.error());
+  }, [slug]);
+
+  if (!singlePost) return <div>Loading...</div>;
+
+  return (
+
+  );
+}
+
+
