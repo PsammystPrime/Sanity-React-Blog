bfbf1ada54e957c02ec08f31b79a627ad135edaf
PsammystPrime <samuelprime384@gmail.com>
2024-04-19 10:51:11 +0300

    filter featured posts

--- my-first-react-app/src/components/Featured.jsx
+++ my-first-react-app/src/components/Featured.jsx
@@ -1,14 +1,73 @@
-import { Nav1, Nav2, Contact, Footer, Return } from "../App"
-export default function All(){
-    return (
-        <main>
-            <Nav1></Nav1>
-            <Nav2></Nav2>
-            <p> Featured Articles </p>
-            <Contact></Contact>
-            <Footer></Footer>
-            <Return></Return>
+import { Nav1, Nav2, Contact, Footer, Return } from "../App";
+import { useState, useEffect } from "react";
+import sanityClient from "../client";
+import { Link } from "react-router-dom";
 
-        </main>
-        )
+export function Post() {
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[categories[]._ref=='c763b87f-0d8e-4b36-be4a-0a71e064495d']{
+        title,
+        slug,
+        mainImage{
+            asset->{
+                _id,
+                url
+            },
+            alt
+        },
+        body,
+        author
+    }`
+      )
+      .then((data) => setPost(data, console.log(data)))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main>
+      <section className="container">
+        <h1>Welcome to my blogs</h1>
+        <h2>Have a great read</h2>
+        <div className="posts">
+          {postData &&
+            postData.map((post, index) => (
+              <article className="article" key={index}>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span key={index}>
+                    <img
+                      className="pic"
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                    />
+                    <span>
+                      <h3 className="title">{post.title}</h3>
+                      <span className="info">
+                        {post.body[0].children[0].text}
+                      </span>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  );
+}
+export default function All() {
+  return (
+    <main>
+      <Nav1></Nav1>
+      <Nav2></Nav2>
+      <p> Featured Articles </p>
+      <Post />
+      <Contact></Contact>
+      <Footer></Footer>
+      <Return></Return>
+    </main>
+  );
 }
